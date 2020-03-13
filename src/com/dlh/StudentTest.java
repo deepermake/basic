f684ef4aae605ef632a905990c9ade3c0cc17f32
lianhai.deng <lianhai.deng@luckincoffee.com>
2020-03-13 14:25:52 +0800

    反射-注解 反射获取类的方法入参

--- src/com/dlh/StudentTest.java
+++ src/com/dlh/StudentTest.java
@@ -1,6 +1,8 @@
 package com.dlh;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,18 +13,29 @@ import java.util.List;
  * @date 2020/3/13 9:35
  */
 public class StudentTest {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+        Student student = new Student();
         // 反射
         Class object = Class.forName("com.dlh.Student");
-        // 获取字段
+        // 获取类字段
         List<Field> fields = Arrays.asList(object.getDeclaredFields());
+        // 获取类方法
+        List<Method> methods = Arrays.asList(object.getDeclaredMethods());
         // 获取注解
         StuAnnotation annotation = (StuAnnotation) object.getAnnotation(StuAnnotation.class);
         System.out.println(object.getName()+" annotation info:"+annotation.value()+", "+annotation.type()+", "+annotation.length());
 
+        // 解析字段注解
         for (Field field:fields){
             System.out.println(field.getName()+":"+field.getAnnotation(StuAnnotation.class).value());
         }
+        // 解析方法
+        for (Method method: methods){
+            if (method.getParameters().length > 0 && method.getName().contains("set") && method.getName().contains("Name")){
+                method.invoke(student, "邓联海");
+            }
+        }
+        System.out.println(student.toString());
 
     }
 }
