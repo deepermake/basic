073244fd15c2cd90af27a750d4756e250ebb151d
deepermake <lianhai.deng@luckincoffee.com>
2020-05-11 15:10:15 +0800

    HashMap 线程安全性

--- src/com/hashmap/Demo.java
+++ src/com/hashmap/Demo.java
@@ -2,33 +2,54 @@ package com.hashmap;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
- *
- * @description
  * @author 邓联海
+ * @description
  * @date 2020/3/20 17:28
  */
 public class Demo {
 
     private static final int MAXIMUM_CAPACITY = 1 << 30;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //多线程验证HashMap的线程安全性
+        Map<Double, Double> map = new HashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Double e = Math.random();
+                    map.put(e, e);
+                    map.remove(e);
+                }
+            }).start();
+        }
+        System.out.println(map.size());
 
-        Map<String, Integer> map = new HashMap<>();
-        map.put("ZhangYi",98);
-        map.put("WangEr",99);
-        map.put("ZhangShan",89);
-        map.put("Lisi",92);
+        //多线程测试ConcurrentHashMap的线程安全性
+        Map<Double, Double> concurrentHashMap = new ConcurrentHashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Double e = Math.random();
+                    concurrentHashMap.put(e, e);
+                    concurrentHashMap.remove(e);
+                }
+            }).start();
+        }
+        System.out.println("ConcurrentHashMap:" + concurrentHashMap.size());
 
-
-        int result = testTableSizeFor(11);
-        System.out.println(result);
 
     }
 
     /**
      * hashMap中取一个大于该长度且最小的2的指数次幂
+     * 初始化hash的初始容量
+     *
      * @param cap
      * @return
      */
