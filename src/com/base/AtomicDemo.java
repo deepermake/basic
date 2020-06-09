9c9ab8073c97e6594551450b1a8fcf05b2ed30ef
deepermake <lianhai.deng@luckincoffee.com>
2020-06-09 17:54:55 +0800

    测试threadLocal

---
+++ src/com/base/AtomicDemo.java
@@ -0,0 +1,16 @@
+package com.base;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * @description 测试Atomic原子类
+ * @author 邓联海
+ * @date 2020/6/9 17:24
+ */
+public class AtomicDemo {
+
+    public static void main(String[] args){
+        AtomicInteger atomicInteger = new AtomicInteger(100);
+    }
+}
---
+++ src/com/threadDemo/ThreadLocalDemo.java
@@ -0,0 +1,10 @@
+package com.threadDemo;
+
+/**
+ *
+ * @description threadLocal测试类
+ * @author 邓联海
+ * @date 2020/6/9 17:50
+ */
+public class ThreadLocalDemo {
+}
