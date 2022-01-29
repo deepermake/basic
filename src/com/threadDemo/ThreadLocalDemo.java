685026c6576bafe14247ba97a9c657fc6ea44701
deepermake <lianhai.deng@luckincoffee.com>
2022-01-29 18:00:36 +0800

    线程池是创建线程的原理

--- src/com/threadDemo/ThreadLocalDemo.java
+++ src/com/threadDemo/ThreadLocalDemo.java
@@ -7,4 +7,15 @@ package com.threadDemo;
  * @date 2020/6/9 17:50
  */
 public class ThreadLocalDemo {
+    public static void main(String[] args) {
+        retry:
+        for (;;){
+            for (int i =0 ;i<5; i++){
+                System.out.println(i);
+                if (i ==4 ){
+                    break retry;
+                }
+            }
+        }
+    }
 }
