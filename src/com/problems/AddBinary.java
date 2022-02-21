dcf17ad4df673cae7d1bb95dc4b8fe17e11026d4
邓联海 <lianhai.deng@luckincoffee.com>
2022-02-21 23:42:45 +0800

    二进制求和

--- src/com/problems/AddBinary.java
+++ src/com/problems/AddBinary.java
@@ -8,26 +8,47 @@ package com.problems;
 
 public class AddBinary {
     public static void main(String[] args) {
-        addBinary("1010", "1011");
+        System.out.println(addBinary("100", "110010"));
     }
 
     public static String addBinary(String a, String b) {
         int i = 1, j = 1;
         int tem = 0;
-        String result = "";
-        while (i < a.length() || j < b.length()) {
-            int aTemp = Integer.parseInt(String.valueOf(a.charAt(a.length() - i)));
-            int bTemp = Integer.parseInt(String.valueOf(b.charAt(b.length() - j)));
-            if (aTemp + bTemp + tem >= 2){
-                result += "0";
-                tem = 1;
-            }else {
-                tem = 0;
-                result += "1";
+        StringBuilder result = new StringBuilder();
+        while (i <= a.length() || j <= b.length()) {
+            int aTemp = 0, bTemp = 0;
+            if (i <= a.length()){
+                aTemp = Integer.parseInt(String.valueOf(a.charAt(a.length() - i)));
+            }
+            if (j <= b.length()){
+                bTemp = Integer.parseInt(String.valueOf(b.charAt(b.length() - j)));
+            }
+            int re = aTemp + bTemp + tem;
+            switch (re){
+                case 0:
+                    if (i <= a.length() && j <= b.length()) {
+                        result.append("0");
+                    }
+                    tem = 0;
+                    break;
+                case 1:
+                    result.append("1");
+                    tem = 0;
+                    break;
+                case 2:
+                    result.append("0");
+                    tem = 1;
+                    break;
+                case 3:
+                    result.append("1");
+                    tem = 1;
             }
             i++;
             j++;
         }
-        return "";
+        if (tem > 0){
+            result.append("1");
+        }
+        return result.reverse().toString();
     }
 }
