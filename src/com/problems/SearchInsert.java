7397b919603385ebf60a0c877fa1539a5807a911
deepermake <lianhai.deng@luckincoffee.com>
2022-03-21 20:31:58 +0800

    实现 SearchInsert

--- src/com/problems/SearchInsert.java
+++ src/com/problems/SearchInsert.java
@@ -11,24 +11,38 @@ public class SearchInsert {
 
     public static void main(String[] args) {
 
-        int[] arr = {0, 1, 2, 4, 5};
+        int[] arr = {1,3,5,6};
 
-        System.out.println(searchInsert(arr, 3));
+        System.out.println(searchInsert(arr, 2));
     }
 
 
     public static int searchInsert(int[] nums, int target) {
-        int len = nums.length;
-
-        if (len == 1) {
-            return 0;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        if (target <= nums[left]) {
+            return left;
         }
-        int startIndex = len / 2;
-        int i = 0;
-        int k = startIndex;
+        if (target >= nums[right]) {
+            return nums.length;
+        }
 
+        if (right - left == 1) {
+            return 1;
+        }
 
-        return i;
+        while (left < right - 1) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            }
+        }
+        return mid;
     }
 
 }
