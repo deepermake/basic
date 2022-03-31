0a32e508c03d6006777fc19c3bf601ae8bb83d29
deepermake <lianhai.deng@luckincoffee.com>
2022-03-31 16:02:28 +0800

    实现 SearchInsert 二分查找算法

--- src/com/problems/SearchInsert.java
+++ src/com/problems/SearchInsert.java
@@ -11,7 +11,7 @@ public class SearchInsert {
 
     public static void main(String[] args) {
 
-        int[] arr = {1,3,5,6};
+        int[] arr = {1};
 
         System.out.println(searchInsert(arr, 2));
     }
@@ -19,30 +19,18 @@ public class SearchInsert {
 
     public static int searchInsert(int[] nums, int target) {
         int left = 0;
-        int right = nums.length - 1;
-        int mid = 0;
-        if (target <= nums[left]) {
-            return left;
-        }
-        if (target >= nums[right]) {
-            return nums.length;
-        }
-
-        if (right - left == 1) {
-            return 1;
-        }
-
-        while (left < right - 1) {
-            mid = (left + right) / 2;
+        int right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else if (nums[mid] > target) {
                 right = mid;
-            } else if (nums[mid] < target) {
-                left = mid;
             }
         }
-        return mid;
+        return left;
     }
 
 }
