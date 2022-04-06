693c39eafd8af5312fc7ffcf3343f26699ca520e
deepermake <2039211458@qq.com>
2022-04-06 18:17:58 +0800

    加一

---
+++ src/com/problems/PlusOne.java
@@ -0,0 +1,16 @@
+package com.problems;
+
+/**
+ * @author lianhai.deng
+ * @description 加一 https://leetcode-cn.com/problems/plus-one/
+ * @date 2022/4/6 17:54
+ */
+public class PlusOne {
+    public static void main(String[] args) {
+        System.out.println(plusOne(new int[]{1, 2, 0, 9}));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        return null;
+    }
+}
