a1250167c788bc31697c303389f810c458ba2c69
witw <2039211458@qq.com>
2022-05-19 23:11:26 +0800

    两个数的交集

--- src/com/problems/Intersection.java
+++ src/com/problems/Intersection.java
@@ -1,18 +1,30 @@
 package com.problems;
 
+import java.util.*;
+
 /**
- * @description 两个数组的交集 
- *              https://leetcode.cn/problems/intersection-of-two-arrays/
+ * @description 两个数组的交集
+ * https://leetcode.cn/problems/intersection-of-two-arrays/
  * @Date 2022/5/19 19:41
  * @Created by lianhai.deng
  */
 public class Intersection {
 
     public static void main(String[] args) {
-
+        intersection(new int[]{4, 9, 2, 1}, new int[]{9, 24, 9, 4});
     }
 
     public static int[] intersection(int[] nums1, int[] nums2) {
-        return null;
+        List<Integer> list = new ArrayList<>();
+        for (int x : nums1) {
+            if (!list.contains(x) && Arrays.stream(nums2).filter(i -> Objects.equals(i, x)).count() > 0) {
+                list.add(x);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
     }
 }
