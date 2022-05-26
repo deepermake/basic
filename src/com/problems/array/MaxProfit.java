acdc485d0bc6a37f2e7ef25adea490c77f466c84
lianhai.deng <lianhai.deng@lkcoffee.com>
2022-05-26 17:48:54 +0800

    121. 买卖股票的最佳时机

---
+++ src/com/problems/array/MaxProfit.java
@@ -0,0 +1,19 @@
+package com.problems.array;
+
+/**
+ * Description: 121. 买卖股票的最佳时机
+ * Link:        https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+ * Author:      lianhai.deng
+ * Date:        2022/5/26 15:07
+ */
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{1, 7, 5, 8, 1, 9}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        return maxProfit;
+    }
+}
