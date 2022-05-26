12ffdcfb33951873238d4f28b679c6ca1989ccf4
lianhai.deng <lianhai.deng@lkcoffee.com>
2022-05-26 11:14:00 +0800

    多数元素

---
+++ src/com/problems/array/MajorityElement.java
@@ -0,0 +1,18 @@
+package com.problems.array;
+
+/**
+ * Description: 多数元素
+ * Link:    https://leetcode.cn/problems/majority-element/
+ * Author:   lianhai.deng
+ * Date:     2022/5/26 11:11
+ */
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{3, 2, 3}));
+    }
+
+    public static int majorityElement(int[] nums) {
+        return 0;
+    }
+}
