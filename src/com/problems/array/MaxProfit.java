6ca1410ab219da7cef6f596b1e52c4c052ddb818
lianhai.deng <lianhai.deng@lkcoffee.com>
2022-05-26 20:28:24 +0800

    121. 买卖股票的最佳时机

--- src/com/problems/array/MaxProfit.java
+++ src/com/problems/array/MaxProfit.java
@@ -9,11 +9,41 @@ package com.problems.array;
 public class MaxProfit {
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{1, 7, 5, 8, 1, 9}));
+        // System.out.println(maxProfit(new int[]{7, 1, 4, 3, 1, 9}));
+        System.out.println(maxProfit2(new int[]{7, 6, 4, 3, 1}));
     }
 
     public static int maxProfit(int[] prices) {
-        int maxProfit = 0;
+        if (prices.length == 1) {
+            return 0;
+        }
+        int begin = 0, end = 1, maxProfit = prices[end] - prices[begin];
+        int k = 1;
+        while (k < prices.length) {
+            if (prices[k] < prices[begin] && prices[k] < prices[end]) {
+                end = k;
+                begin = k;
+            } else if (prices[k] > prices[begin]) {
+                end = k;
+            } else if (prices[k] < prices[begin]) {
+                begin = end;
+                end = k;
+            }
+            maxProfit = Math.max(maxProfit, prices[end] - prices[begin]);
+            k++;
+        }
         return maxProfit;
+    }
+
+    public static int maxProfit2(int[] prices) {
+        if (prices.length == 1) {
+            return 0;
+        }
+        int min = prices[0], max = 0;
+        for (int x : prices) {
+            max = Math.max(max, x - min);
+            min = Math.min(min, x);
+        }
+        return max;
     }
 }
