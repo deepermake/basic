2d2bdd357c3b0321478bf2045b7cf0f4ccb98442
lianhai.deng <lianhai.deng@lkcoffee.com>
2022-06-06 18:03:48 +0800

    1260 二维网格迁移

---
+++ src/com/base/sort/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.base.sort;
+
+/**
+ * Description: 冒泡排序
+ * Link:
+ * Author:   lianhai.deng
+ * Date:     2022/5/31 17:19
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 3, 2, 5, 0, 2};
+        bubbleSort(arr);
+    }
+
+
+    private static void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+
+}
---
+++ src/com/base/sort/QuickSort.java
@@ -0,0 +1,19 @@
+package com.base.sort;
+
+/**
+ * Description: 快速排序
+ * Link:
+ * Author:   lianhai.deng
+ * Date:     2022/5/31 17:56
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 3, 2, 5, 0, 2};
+        quickSort(arr);
+    }
+
+    private static void quickSort(int[] arr) {
+
+    }
+}
