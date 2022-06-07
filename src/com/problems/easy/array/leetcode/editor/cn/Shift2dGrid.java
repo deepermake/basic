38b4d11c2344f906af58fef3f3ee1c4cd17d8fa5
lianhai.deng <lianhai.deng@lkcoffee.com>
2022-06-07 18:30:22 +0800

    1260 二维网格迁移

--- src/com/problems/easy/array/leetcode/editor/cn/Shift2dGrid.java
+++ src/com/problems/easy/array/leetcode/editor/cn/Shift2dGrid.java
@@ -12,7 +12,6 @@ import java.util.List;
 public class Shift2dGrid {
     public static void main(String[] args) {
         Solution solution = new Shift2dGrid().new Solution();
-
         int[][] grid = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int k = 1;
         solution.shiftGrid(grid, k);
@@ -23,6 +22,26 @@ public class Shift2dGrid {
         public List<List<Integer>> shiftGrid(int[][] grid, int k) {
             List<List<Integer>> list = new ArrayList<>();
 
+            int y = grid.length;
+            int x = grid[0].length;
+            int nums = Math.multiplyExact(x, y);
+            if (k > nums) {
+                k = k / nums;
+            }
+
+            for (int i = 0; i < y; i++) {
+                for (int j = 0; j < x; j++) {
+                    grid[(i + k) / y][(j + k) / x] = grid[i][j];
+                }
+            }
+
+            for (int i = 0; i < y; i++) {
+                List<Integer> re = new ArrayList<>();
+                for (int j = 0; j < x; j++) {
+                    re.add(grid[i][j]);
+                }
+                list.add(re);
+            }
 
 
             return list;
