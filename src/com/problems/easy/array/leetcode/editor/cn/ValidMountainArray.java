9103bc4cf004a576b247acae39acefd3d94c1150
witw <2039211458@qq.com>
2022-06-08 23:14:04 +0800

    941. 有效的山脉数组

--- src/com/problems/easy/array/leetcode/editor/cn/ValidMountainArray.java
+++ src/com/problems/easy/array/leetcode/editor/cn/ValidMountainArray.java
@@ -1,22 +1,47 @@
 package com.problems.easy.array.leetcode.editor.cn;
 
 /**
- * Description: 941 有效的山脉数组
+ * Description: 941. 有效的山脉数组
  * Link:
  * Author:      lianhai.deng
- * Date:        2022-06-08 18:03:31
+ * Date:        2022-06-08 22:18:27
  */
 public class ValidMountainArray {
     public static void main(String[] args) {
         Solution solution = new ValidMountainArray().new Solution();
-        int[] arr = new int[]{3, 5, 6, 3, 2};
+        int[] arr = new int[]{0, 3, 4, 9, 5, 2, 6};
         solution.validMountainArray(arr);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean validMountainArray(int[] arr) {
-            return false;
+            if (arr.length <= 2) {
+                return false;
+            }
+
+            if (arr[0] >= arr[1]) {
+                return false;
+            }
+
+            int len = arr.length;
+            int index = 0;
+            for (int i = 1; i < len; i++) {
+                if (arr[i - 1] == arr[i]){
+                    return false;
+                }
+                if (arr[i - 1] > arr[i]) {
+                    index = i;
+                    break;
+                }
+            }
+
+            for (int j = index + 1; j < len; j++) {
+                if (arr[j - 1] <= arr[j]) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
