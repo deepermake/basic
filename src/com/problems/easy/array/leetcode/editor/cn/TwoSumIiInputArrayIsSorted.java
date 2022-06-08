7c336588d279affebed8453b9f46849f0070e4f6
witw <2039211458@qq.com>
2022-06-08 23:29:41 +0800

    167 两数之和 II - 输入有序数组

---
+++ src/com/problems/easy/array/leetcode/editor/cn/TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,25 @@
+package com.problems.easy.array.leetcode.editor.cn;
+
+/**
+ * Description: 167 两数之和 II - 输入有序数组
+ * Link:
+ * Author:      lianhai.deng
+ * Date:        2022-06-08 23:24:51
+ */
+public class TwoSumIiInputArrayIsSorted {
+    public static void main(String[] args) {
+        Solution solution = new TwoSumIiInputArrayIsSorted().new Solution();
+        int[] nums = new int[]{2, 7, 11, 15};
+        solution.twoSum(nums, 9);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
