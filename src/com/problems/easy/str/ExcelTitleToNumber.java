9305b875c43e6f5472d0c3b80183d57502cd3a52
denglianhai <denglianhai@genomics.cn>
2023-12-20 17:15:46 +0800

    leetcode - ExcelTitleToNumber Excel表列名称

---
+++ src/com/problems/easy/str/ExcelTitleToNumber.java
@@ -0,0 +1,37 @@
+package com.problems.easy.str;
+
+/**
+ * Excel 表列名称
+ */
+public class ExcelTitleToNumber {
+
+    public static void main(String[] args) {
+        System.out.println('Z'-'A' + 1);
+        // 1
+        System.out.println(titleToNumber("A"));
+        // 28
+        System.out.println(titleToNumber("AB"));
+        // 701
+        System.out.println(titleToNumber("ZY"));
+        // 701
+        System.out.println(titleToNumber("AAE"));
+        // 2147483647
+        System.out.println(titleToNumber("FXSHRXW")); //
+    }
+
+    public static int titleToNumber(String columnTitle) {
+        if (columnTitle.length() == 1) {
+            return columnTitle.charAt(0) - 'A' + 1;
+        }
+        // 结果
+        int result = columnTitle.charAt(0) - 'A' + 1;
+        int index = 1;
+
+        while (index < columnTitle.length()) {
+            char value = columnTitle.charAt(index);
+            result = result * 26 + (value - 'A' + 1);
+            index ++;
+        }
+        return result;
+    }
+}
