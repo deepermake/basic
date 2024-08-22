19ac35e70783b0f82266f4de20420d95475d75c1
denglianhai <denglianhai@genomics.cn>
2024-08-22 16:34:24 +0800

    leetcode - 18-四数之和

--- src/com/problems/str/mid/FourSum.java
+++ src/com/problems/str/mid/FourSum.java
@@ -9,7 +9,8 @@ public class FourSum {
 
     public static void main(String[] args) {
         // System.out.println(fourSum(new int[]{2, 2, 2, 2, 2}, 8));
-        System.out.println(fourSum(new int[]{1000000000, 1000000000, 1000000000, 1000000000}, -299999999));
+        // System.out.println(fourSum(new int[]{1000000000, 1000000000, 1000000000, 1000000000}, -299999999));
+        System.out.println(fourSum(new int[]{4,-9,-2,-2,-7,9,9,5,10,-10,4,5,2,-4,-2,4,-9,5}, -13));
     }
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
@@ -20,31 +21,46 @@ public class FourSum {
         Set<String> set = new HashSet<>();
         // 排序
         Arrays.sort(nums);
+
         // 固定第一个
         for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            if (target > 0 && nums[i] > target) {
+                return lists;
+            }
+
             // 固定第二个
             for (int j = i + 1; j < nums.length - 1; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
                 // 双指针互相移动，得到结果
                 int k = j + 1;
                 int m = nums.length - 1;
                 while (k < m) {
-                    int res = nums[i] + nums[j] + nums[k];
-                    int temp = target - nums[m];
-                    String key = nums[i] + "_" + nums[j] + "_" + nums[k] + "_" + nums[m];
-                    if (res == temp && !set.contains(key)) {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        list.add(nums[m]);
-                        lists.add(list);
-                        set.add(key);
+                    long res = (long) nums[i] + nums[j] + nums[k] + nums[m];
+                    if (res > target) {
+                        m--;
+                        continue;
                     }
-                    if (res > temp) {
+                    if (res < target) {
                         k++;
-                    } else {
+                        continue;
+                    }
+                    lists.add(Arrays.asList(nums[i], nums[j], nums[k], nums[m]));
+                    while (m > k + 1 && nums[m] == nums[m - 1]) {
                         m--;
                     }
+                    while (k + 1 < m && nums[k] == nums[k + 1]) {
+                        k++;
+                    }
+                    m--;
+                    k++;
                 }
             }
         }
