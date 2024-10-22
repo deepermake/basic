054dee5fee76ec6a42173a0f42b7f582ba710e31
denglianhai <denglianhai@genomics.cn>
2024-10-22 17:29:42 +0800

    leetcode - 面试题 01.07 旋转矩阵

--- src/com/interviewer/Rotate.java
+++ src/com/interviewer/Rotate.java
@@ -8,20 +8,21 @@ public class Rotate {
     public static void main(String[] args) {
 
         // int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int[][] matrix = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        int[][] matrix = new int[][]{{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}, {25, 26, 27, 28, 29, 30}, {31, 32, 33, 34, 35, 36}};
         rotate(matrix);
         System.out.println(matrix);
     }
 
     public static void rotate(int[][] matrix) {
-        int l = matrix[0].length / 2;
+        int n = matrix[0].length;
+        int l = n / 2;
         for (int i = 0; i < l; i++) {
             for (int j = i; j < matrix[i].length - i - 1; j++) {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[l - j + 1][i];
-                matrix[l - j + 1][i] = matrix[l - i + 1][l - j + 1];
-                matrix[l - i + 1][l - j + 1] = matrix[j][l - i + 1];
-                matrix[j][l - i + 1] = temp;
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
             }
         }
     }
