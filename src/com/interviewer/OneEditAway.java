636aa44667410cff2a43cc54813eb6ae6c484fae
denglianhai <denglianhai@genomics.cn>
2024-10-25 14:59:25 +0800

    动态规划解题思路

--- src/com/interviewer/OneEditAway.java
+++ src/com/interviewer/OneEditAway.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class OneEditAway {
 
     public static void main(String[] args) {
-        System.out.println(oneEditAway("ab", "ba"));
+        System.out.println(oneEditAway("p", "l"));
     }
 
     public static boolean oneEditAway(String first, String second) {
@@ -14,37 +14,9 @@ public class OneEditAway {
             return true;
         }
 
-        if (Math.abs(first.length() - second.length()) > 1) {
-            return false;
-        }
 
-        int time = 0;
-        int i = 0, j = 0;
-        while (i < first.length() && j < second.length()) {
-            if (first.charAt(i) == second.charAt(j)) {
-                i++;
-                j++;
-                continue;
-            }
 
-            if (j < second.length() - 1 && first.charAt(i) == second.charAt(j + 1)) {
-                time++;
-                i++;
-                j += 2;
-                continue;
-            }
-
-            if (i < first.length() - 1 && first.charAt(i+1) == second.charAt(j)){
-                time++;
-                i += 2;
-                j++;
-                continue;
-            }
-            time += 2;
-        }
-
-        return time <= 1;
-
+        return false;
 
     }
 }
