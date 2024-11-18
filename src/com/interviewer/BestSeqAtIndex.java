b6c49ed2515490bfb682dbe8403caaebad3375bd
denglianhai <denglianhai@genomics.cn>
2024-11-18 17:57:04 +0800

    leetcode - 面试题 17.08 马戏团人塔 doing

--- src/com/interviewer/BestSeqAtIndex.java
+++ src/com/interviewer/BestSeqAtIndex.java
@@ -2,9 +2,13 @@ package com.interviewer;
 
 import java.util.*;
 
+/**
+ * 面试题 17.08 马戏团人塔
+ */
 public class BestSeqAtIndex {
     public static void main(String[] args) {
-        System.out.println(bestSeqAtIndex2(new int[]{1, 2, 3, 2, 3, 2}, new int[]{7, 3, 3, 4, 5, 7}));
+        // System.out.println(bestSeqAtIndex2(new int[]{1, 2, 3, 2, 3, 2}, new int[]{7, 3, 3, 4, 5, 7}));
+        System.out.println(bestSeqAtIndex2(new int[]{8378, 8535, 8998, 3766, 648, 6184, 5506, 5648, 3907, 6773}, new int[]{9644, 849, 3232, 3259, 5229, 314, 5593, 9600, 6695, 4340}));
         // System.out.println(bestSeqAtIndex2(new int[]{65}, new int[]{100}));
     }
 
@@ -62,10 +66,18 @@ public class BestSeqAtIndex {
             arr[i][0] = height[i];
             arr[i][1] = weight[i];
         }
-        Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(arr, Comparator.comparingInt(o -> -o[0]));
+        // dp[i] 表示以 nums[i] 结尾的最长递增子序列的长度
         int[] dp = new int[l];
-        dp[0] = 1;
-        int max = 1;
-        return max;
+        for (int i = 1; i < l; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+
+            }
+        }
+        Arrays.sort(dp);
+        return dp[l - 1] + 1;
     }
 }
