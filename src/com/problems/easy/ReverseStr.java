900b040822cb5a364104ab4d29b7ffbc74ef2d39
denglianhai <denglianhai@genomics.cn>
2024-11-26 15:11:58 +0800

    leetcode - 541 - 反转字符串II

--- src/com/problems/easy/ReverseStr.java
+++ src/com/problems/easy/ReverseStr.java
@@ -1,43 +1,37 @@
 package com.problems.easy;
 
+/**
+ * 541 - 反转字符串II
+ */
 public class ReverseStr {
     public static void main(String[] args) {
 
-        System.out.println(reverseStr("abcdefg", 2));
+        System.out.println(reverseStr("abcdefgh", 3));
 
     }
 
+    /**
+     * 思路：
+     *
+     * @param s
+     * @param k
+     * @return
+     */
     public static String reverseStr(String s, int k) {
-        if (s.length() < 2 * k) {
-            return s;
-        }
-
-        int start = 0, end = 2 * k;
         StringBuilder result = new StringBuilder();
-        int left = 2 * k;
-        while (left < s.length()) {
-
-            result.append(new StringBuffer(s.substring(start, k)).reverse());
-            result.append(s, start + k, 2 * k);
-
-            left = s.length() - end;
-            if (left == 0) {
-                break;
-            }
-
-            if (left > 2 * k) {
-                start = end;
-                end += 2 * k;
+        int start = 0;
+        int left = s.length();
+        while (left > 0) {
+            // 如果超过2k个字符，就反转2k个字符的前k个字符
+            if (left >= k) {
+                // [start, start+k]反转，[start+k+1, start+2*k]不反转
+                int end = left >= 2 * k ? start + 2 * k : start + left;
+                result.append(new StringBuffer(s.substring(start, start + k)).reverse()).append(s, start + k, end);
+                start = start + 2 * k;
+                left = left - (2 * k);
                 continue;
             }
-
-            if (left >= k) {
-                result.append(new StringBuffer(s.substring(start + k, k)).reverse());
-            }
-
-            if (left < k) {
-                result.append(new StringBuffer(s.substring(end)).reverse());
-            }
+            result.append(new StringBuffer(s.substring(start)).reverse());
             break;
         }
         return result.toString();
