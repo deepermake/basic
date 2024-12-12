a650a355f4b816ef3e48dc61d0f66e2792e0f42c
denglianhai <denglianhai@genomics.cn>
2024-12-12 17:24:04 +0800

    leetcode - 45 - 跳跃游戏||

--- src/com/problems/mid/Jump.java
+++ src/com/problems/mid/Jump.java
@@ -1,9 +1,13 @@
 package com.problems.mid;
 
+/**
+ * 45 - 跳跃游戏||
+ */
 public class Jump {
 
     public static void main(String[] args) {
-        System.out.println(jump(new int[]{2,2,5,6,7,1,4,5,8}));
+        System.out.println(jump2(new int[]{2, 2, 5, 6, 7, 1, 4, 5, 8}));
+        System.out.println(jump2(new int[]{2, 3, 1, 1, 4}));
     }
 
 
@@ -56,11 +60,11 @@ public class Jump {
      * dp[i][j] 表示从i到j的最小跳跃数
      *
      * @param nums
-     * @return
+     * @return Memory over
      */
     public static int jump(int[] nums) {
 
-        if (nums.length == 1){
+        if (nums.length == 1) {
             return 0;
         }
 
@@ -76,7 +80,7 @@ public class Jump {
                     dp[i][j] = 0;
                     continue;
                 }
-                if (i == j){
+                if (i == j) {
                     dp[i][j] = lastSmallStep;
                     continue;
                 }
@@ -113,11 +117,53 @@ public class Jump {
         }
 
         for (int j = 0; j < i; j++) {
-            if (dp[j][i] == 0){
+            if (dp[j][i] == 0) {
                 continue;
             }
             result = Math.min(dp[j][i], result);
         }
         return result;
+    }
+
+
+    /**
+     * 贪心算法
+     *
+     * 从结果出发，最后一个坐标反向向前跳跃
+     * 每一次尽量跳的远，直到到达坐标为0的位置
+     * 从len - 2的位置，开始比对，直到最后一个可以跳跃到len - 1的坐标len - x
+     * 从len - x - 1的位置，开始比对，直到最后一个可以跳跃到len - x的坐标为len - x'
+     * 直到len - x' = 0 结束
+     *
+     * @param nums
+     * @return
+     */
+    public static int jump2(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int target = nums.length - 1;
+        int start = nums.length - 2;
+        int temp = 0;
+        int time = 0;
+        while (target != 0) {
+            // 坐标start可以跳跃到target
+            if (nums[start] >= target - start) {
+                temp = start;
+            }
+
+            // 坐标start不能跳跃到target
+            if (start != 0) {
+                start--;
+                continue;
+            }
+            target = temp;
+            start = temp - 1;
+            time++;
+
+        }
+        return time;
+
+
     }
 }
