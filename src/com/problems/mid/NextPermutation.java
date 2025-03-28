e92b580d85ef59dd40afdb3d9a40101538d5a822
denglianhai <denglianhai@genomics.cn>
2025-03-28 15:28:45 +0800

    leetcode - 31 - 下一个排列

--- src/com/problems/mid/NextPermutation.java
+++ src/com/problems/mid/NextPermutation.java
@@ -2,20 +2,24 @@ package com.problems.mid;
 
 import java.util.Arrays;
 
+
+/**
+ * 31 - 下一个排列
+ */
 public class NextPermutation {
 
     public static void main(String[] args) {
         //nextPermutation(new int[]{1, 2, 3});
-        nextPermutation(new int[]{4, 6, 7, 2, 6, 1, 3, 2});
+        // nextPermutation(new int[]{4, 6, 7, 2, 6, 1, 3, 2});
+        // nextPermutation(new int[]{1, 3, 2, 2, 2, 2, 2});
+        // nextPermutation(new int[]{1, 2, 3});
+        // nextPermutation(new int[]{3, 2, 1});
+        nextPermutation(new int[]{1,1,5});
     }
 
     /**
      * 双指针解法
-     * end指针为 nums.length - 1;
-     * <p>
-     * 下标，下标对应的值
-     * <p>
-     * 只要从尾巴开始关注3个数
+     * [1,3,2,1] 找到 [1, 3]
      *
      * @param nums
      */
@@ -23,11 +27,45 @@ public class NextPermutation {
         if (nums.length == 1) {
             return;
         }
-        // 慢指针，数组尾巴
         int end = nums.length - 1;
-        // 快指针，
+        int start = end - 1;
+        boolean flag = false;
+        while (start >= 0) {
+            if (nums[start] < nums[end]) {
+                flag = true;
+                break;
+            }
+            start--;
+            end--;
+        }
 
+        if (!flag) {
+            Arrays.sort(nums);
+            return;
+        }
 
-        Arrays.sort(nums);
+        int index = getMin(nums[start], nums, end);
+        int temp = nums[index];
+        nums[index] = nums[start];
+        nums[start] = temp;
+
+        Arrays.sort(nums, end, nums.length);
+    }
+
+    /**
+     * 从[nums[i] ~ nums.len-1] 中找到一个比num大，且在集合中最小的数
+     *
+     * @param num
+     * @param nums
+     * @param i
+     * @return
+     */
+    public static int getMin(int num, int[] nums, int i) {
+        for (int l = nums.length - 1; l > i; l--) {
+            if (nums[l] > num) {
+                return l;
+            }
+        }
+        return i;
     }
 }
