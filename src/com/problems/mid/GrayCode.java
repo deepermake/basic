bdf1b659b5d206e8f6226d6c697f129b1c251bf3
denglianhai <denglianhai@genomics.cn>
2026-07-15 17:50:46 +0800

    leetcode - 89 - 格雷编码 暴力破解法

--- src/com/problems/mid/GrayCode.java
+++ src/com/problems/mid/GrayCode.java
@@ -2,6 +2,7 @@ package com.problems.mid;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class GrayCode {
 
@@ -10,38 +11,36 @@ public class GrayCode {
         System.out.println(ob.grayCode(16));
     }
 
-    List<Integer> list = new ArrayList<>();
-    boolean found = false;
-
     public List<Integer> grayCode(int n) {
-        list.add(0);
-        backtracking(n, 0);
+        List<Integer> list = new ArrayList<>();
+        int temp = 0, k = 0;
+        list.add(temp);
+        int len = (int) Math.pow(2, n);
+        while (list.size() < len) {
+            for (int i = 0; i < n; i++) {
+                if (k != 0 && k == i) {
+                    continue;
+                }
+                // 取下一个数
+                int num = temp ^ (1 << i);
+                if (list.contains(num) ||
+                        (list.size() == len - 1 && (num & (num - 1)) != 0)) {
+                    continue;
+                }
+                list.add(num);
+                temp = num;
+                k = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+            }
+        }
+
         return list;
-    }
-
-    public void backtracking(int n, int cur) {
-        if (found) {
-            return;
-        }
-        // 终止条件
-        if (list.size() == (1 << n)
-                && diff_one_bit(0, list.get(list.size() - 1))) {
-            found = true;
-            return;
-        }
-
-        for (int i = 0; i < (1 << n); i++) {
-            int num = fit_bit(cur, i);
-            if (list.contains(num)) {
-                continue;
-            }
-            list.add(num);
-            backtracking(n, num);
-            if (found) {
-                return;
-            }
-            list.remove(list.size() - 1);
-        }
     }
 
     /**
@@ -53,17 +52,5 @@ public class GrayCode {
      */
     public Integer fit_bit(int num, int n) {
         return num ^ (1 << n);
-    }
-
-    /**
-     * 是否只有1位不同
-     *
-     * @param a
-     * @param b
-     * @return
-     */
-    public boolean diff_one_bit(int a, int b) {
-        int xor = a ^ b;
-        return xor != 0 && (xor & (xor - 1)) == 0;
     }
 }
