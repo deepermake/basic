7adc44e3115bf2037ebf5753678b35de878246bf
denglianhai <denglianhai@genomics.cn>
2026-07-28 16:34:19 +0800

    leetcode - 89 - 格雷编码 根据格雷编码逆排序解法

--- src/com/problems/mid/GrayCode.java
+++ src/com/problems/mid/GrayCode.java
@@ -2,44 +2,24 @@ package com.problems.mid;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class GrayCode {
 
     public static void main(String[] args) {
         GrayCode ob = new GrayCode();
-        System.out.println(ob.grayCode(16));
+        System.out.println(ob.grayCode(1));
     }
 
     public List<Integer> grayCode(int n) {
         List<Integer> list = new ArrayList<>();
-        int temp = 0, k = 0;
-        list.add(temp);
-        int len = (int) Math.pow(2, n);
-        while (list.size() < len) {
-            for (int i = 0; i < n; i++) {
-                if (k != 0 && k == i) {
-                    continue;
-                }
-                // 取下一个数
-                int num = temp ^ (1 << i);
-                if (list.contains(num) ||
-                        (list.size() == len - 1 && (num & (num - 1)) != 0)) {
-                    continue;
-                }
-                list.add(num);
-                temp = num;
-                k = i;
-                break;
+        list.add(0);
+        for (int k = 0; k < n; k++) {
+            int j = list.size();
+            for (int i = j - 1; i >= 0; i--) {
+                list.add(list.get(i) | (1 << k));
             }
+
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-
-            }
-        }
-
         return list;
     }
 
